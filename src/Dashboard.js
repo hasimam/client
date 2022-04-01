fc340504064eb0268b6e79e27b36dfb5516c058a
Hasan Alimam <hasan.alimam@christiedigital.com>
2022-04-01 15:24:24 +0200

    Implement search and SpotifyPlayBack

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import { Container, Form } from "react-bootstrap"
 import useAuth from "./useAuth"
+import TrackSearchResult from './TrackSearchResult'
+import Player from "./Player"
 import SpotifyWebApi from "spotify-web-api-node"
 
 
@@ -12,6 +14,12 @@ export default function Dashboard({ code }) {
     const accessToken = useAuth(code)
     const [search, setSearch] = useState("")
     const [searchResults, setSearchResults] = useState([])
+    const [playingTrack, setPlayingTrack] = useState()
+
+    function chooseTrack(track) {
+        setPlayingTrack(track)
+        setSearch('')
+    }
 
     useEffect(() => {
         if (!accessToken) return
@@ -21,10 +29,26 @@ export default function Dashboard({ code }) {
     useEffect(() => {
         if (!search) return setSearchResults([])
         if (!accessToken) return
+
+        let cancel = false
         spotifyApi.searchTracks(search).then(response => {
-            console.log(response)
+            if (cancel) return
+            setSearchResults(response.body.tracks.items.map(track => {
+                const smallestAlbumImage = track.album.images.reduce((smallest, image) => {
+                    if (image.height < smallest.height) return image
+                    return smallest
+                }, track.album.images[0])
+
+                return {
+                    artist: track.artists[0].name,
+                    title: track.name,
+                    uri: track.uri,
+                    albumUrl: smallestAlbumImage.url,
+                }
+            }))
         })
-    }, [search])
+        return () => cancel = true
+    }, [search, accessToken])
 
     return (
         <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
@@ -33,10 +57,11 @@ export default function Dashboard({ code }) {
                 placeholder="Search Songs/Artists"
                 value={search}
                 onChange={e => setSearch(e.target.value)} />
-            <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>Hi</div>
-            <div>
-                HiHi
+            <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>{
+                searchResults.map(track => <TrackSearchResult track={track} key={track.uri} chooseTrack={chooseTrack} />)}
+
             </div>
+            <div><Player accessToken={accessToken} trackUri={playingTrack?.uri} /></div>
         </Container>
     )
 }
